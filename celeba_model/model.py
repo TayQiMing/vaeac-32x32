f27fb00db6da9700c3b939701b365124167704bd
Qiming <99074858+TayQiMing@users.noreply.github.com>
2022-12-02 14:56:48 +0800

    Update model.py
    modify to layer for 32x32 cifar10 image size

--- celeba_model/model.py
+++ celeba_model/model.py
@@ -33,6 +33,8 @@ def MLPBlock(dim):
         nn.Conv2d(dim, dim, 1)
     )
 
+######### modify code #########
+
 proposal_network = nn.Sequential(
     nn.Conv2d(6, 8, 1),
     ResBlock(8, 8), ResBlock(8, 8), ResBlock(8, 8), ResBlock(8, 8),
@@ -43,15 +45,7 @@ proposal_network = nn.Sequential(
     nn.AvgPool2d(2, 2), nn.Conv2d(16, 32, 1),
     ResBlock(32, 16), ResBlock(32, 16), ResBlock(32, 16), ResBlock(32, 16),
     nn.AvgPool2d(2, 2), nn.Conv2d(32, 64, 1),
-    ResBlock(64, 32), ResBlock(64, 32), ResBlock(64, 32), ResBlock(64, 32),
-    nn.AvgPool2d(2, 2), nn.Conv2d(64, 128, 1),
-    ResBlock(128, 64), ResBlock(128, 64),
-    ResBlock(128, 64), ResBlock(128, 64),
-    nn.AvgPool2d(2, 2), nn.Conv2d(128, 256, 1),
-    ResBlock(256, 128), ResBlock(256, 128),
-    ResBlock(256, 128), ResBlock(256, 128),
-    nn.AvgPool2d(2, 2), nn.Conv2d(256, 512, 1),
-    MLPBlock(512), MLPBlock(512), MLPBlock(512), MLPBlock(512),
+    MLPBlock(64), MLPBlock(64), MLPBlock(64), MLPBlock(64),
 )
 
 prior_network = nn.Sequential(
@@ -69,33 +63,11 @@ prior_network = nn.Sequential(
     ResBlock(32, 16), ResBlock(32, 16), ResBlock(32, 16), ResBlock(32, 16),
     MemoryLayer('#4'),
     nn.AvgPool2d(2, 2), nn.Conv2d(32, 64, 1),
-    ResBlock(64, 32), ResBlock(64, 32), ResBlock(64, 32), ResBlock(64, 32),
-    MemoryLayer('#5'),
-    nn.AvgPool2d(2, 2), nn.Conv2d(64, 128, 1),
-    ResBlock(128, 64), ResBlock(128, 64),
-    ResBlock(128, 64), ResBlock(128, 64),
-    MemoryLayer('#6'),
-    nn.AvgPool2d(2, 2), nn.Conv2d(128, 256, 1),
-    ResBlock(256, 128), ResBlock(256, 128),
-    ResBlock(256, 128), ResBlock(256, 128),
-    MemoryLayer('#7'),
-    nn.AvgPool2d(2, 2), nn.Conv2d(256, 512, 1),
-    MLPBlock(512), MLPBlock(512), MLPBlock(512), MLPBlock(512),
+    MLPBlock(64), MLPBlock(64), MLPBlock(64), MLPBlock(64),
 )
 
 generative_network = nn.Sequential(
-    nn.Conv2d(256, 256, 1),
-    MLPBlock(256), MLPBlock(256), MLPBlock(256), MLPBlock(256),
-    nn.Conv2d(256, 128, 1), nn.Upsample(scale_factor=2),
-    MemoryLayer('#7', True), nn.Conv2d(384, 128, 1),
-    ResBlock(128, 64), ResBlock(128, 64),
-    ResBlock(128, 64), ResBlock(128, 64),
-    nn.Conv2d(128, 64, 1), nn.Upsample(scale_factor=2),
-    MemoryLayer('#6', True), nn.Conv2d(192, 64, 1),
-    ResBlock(64, 32), ResBlock(64, 32), ResBlock(64, 32), ResBlock(64, 32),
-    nn.Conv2d(64, 32, 1), nn.Upsample(scale_factor=2),
-    MemoryLayer('#5', True), nn.Conv2d(96, 32, 1),
-    ResBlock(32, 16), ResBlock(32, 16), ResBlock(32, 16), ResBlock(32, 16),
+
     nn.Conv2d(32, 16, 1), nn.Upsample(scale_factor=2),
     MemoryLayer('#4', True), nn.Conv2d(48, 16, 1),
     ResBlock(16, 8), ResBlock(16, 8), ResBlock(16, 8), ResBlock(16, 8),
@@ -112,3 +84,85 @@ generative_network = nn.Sequential(
     ResBlock(8, 8), ResBlock(8, 8), ResBlock(8, 8), ResBlock(8, 8),
     nn.Conv2d(8, 6, 1),
 )
+
+
+## original code
+# proposal_network = nn.Sequential(
+#     nn.Conv2d(6, 8, 1),
+#     ResBlock(8, 8), ResBlock(8, 8), ResBlock(8, 8), ResBlock(8, 8),
+#     nn.AvgPool2d(2, 2),
+#     ResBlock(8, 8), ResBlock(8, 8), ResBlock(8, 8), ResBlock(8, 8),
+#     nn.AvgPool2d(2, 2), nn.Conv2d(8, 16, 1),
+#     ResBlock(16, 8), ResBlock(16, 8), ResBlock(16, 8), ResBlock(16, 8),
+#     nn.AvgPool2d(2, 2), nn.Conv2d(16, 32, 1),
+#     ResBlock(32, 16), ResBlock(32, 16), ResBlock(32, 16), ResBlock(32, 16),
+#     nn.AvgPool2d(2, 2), nn.Conv2d(32, 64, 1),
+#     ResBlock(64, 32), ResBlock(64, 32), ResBlock(64, 32), ResBlock(64, 32),
+#     nn.AvgPool2d(2, 2), nn.Conv2d(64, 128, 1),
+#     ResBlock(128, 64), ResBlock(128, 64),
+#     ResBlock(128, 64), ResBlock(128, 64),
+#     nn.AvgPool2d(2, 2), nn.Conv2d(128, 256, 1),
+#     ResBlock(256, 128), ResBlock(256, 128),
+#     ResBlock(256, 128), ResBlock(256, 128),
+#     nn.AvgPool2d(2, 2), nn.Conv2d(256, 512, 1),
+#     MLPBlock(512), MLPBlock(512), MLPBlock(512), MLPBlock(512),
+# )
+
+# prior_network = nn.Sequential(
+#     MemoryLayer('#0'),
+#     nn.Conv2d(6, 8, 1),
+#     ResBlock(8, 8), ResBlock(8, 8), ResBlock(8, 8), ResBlock(8, 8),
+#     MemoryLayer('#1'),
+#     nn.AvgPool2d(2, 2),
+#     ResBlock(8, 8), ResBlock(8, 8), ResBlock(8, 8), ResBlock(8, 8),
+#     MemoryLayer('#2'),
+#     nn.AvgPool2d(2, 2), nn.Conv2d(8, 16, 1),
+#     ResBlock(16, 8), ResBlock(16, 8), ResBlock(16, 8), ResBlock(16, 8),
+#     MemoryLayer('#3'),
+#     nn.AvgPool2d(2, 2), nn.Conv2d(16, 32, 1),
+#     ResBlock(32, 16), ResBlock(32, 16), ResBlock(32, 16), ResBlock(32, 16),
+#     MemoryLayer('#4'),
+#     nn.AvgPool2d(2, 2), nn.Conv2d(32, 64, 1),
+#     ResBlock(64, 32), ResBlock(64, 32), ResBlock(64, 32), ResBlock(64, 32),
+#     MemoryLayer('#5'),
+#     nn.AvgPool2d(2, 2), nn.Conv2d(64, 128, 1),
+#     ResBlock(128, 64), ResBlock(128, 64),
+#     ResBlock(128, 64), ResBlock(128, 64),
+#     MemoryLayer('#6'),
+#     nn.AvgPool2d(2, 2), nn.Conv2d(128, 256, 1),
+#     ResBlock(256, 128), ResBlock(256, 128),
+#     ResBlock(256, 128), ResBlock(256, 128),
+#     MemoryLayer('#7'),
+#     nn.AvgPool2d(2, 2), nn.Conv2d(256, 512, 1),
+#     MLPBlock(512), MLPBlock(512), MLPBlock(512), MLPBlock(512),
+# )
+
+# generative_network = nn.Sequential(
+#     nn.Conv2d(256, 256, 1),
+#     MLPBlock(256), MLPBlock(256), MLPBlock(256), MLPBlock(256),
+#     nn.Conv2d(256, 128, 1), nn.Upsample(scale_factor=2),
+#     MemoryLayer('#7', True), nn.Conv2d(384, 128, 1),
+#     ResBlock(128, 64), ResBlock(128, 64),
+#     ResBlock(128, 64), ResBlock(128, 64),
+#     nn.Conv2d(128, 64, 1), nn.Upsample(scale_factor=2),
+#     MemoryLayer('#6', True), nn.Conv2d(192, 64, 1),
+#     ResBlock(64, 32), ResBlock(64, 32), ResBlock(64, 32), ResBlock(64, 32),
+#     nn.Conv2d(64, 32, 1), nn.Upsample(scale_factor=2),
+#     MemoryLayer('#5', True), nn.Conv2d(96, 32, 1),
+#     ResBlock(32, 16), ResBlock(32, 16), ResBlock(32, 16), ResBlock(32, 16),
+#     nn.Conv2d(32, 16, 1), nn.Upsample(scale_factor=2),
+#     MemoryLayer('#4', True), nn.Conv2d(48, 16, 1),
+#     ResBlock(16, 8), ResBlock(16, 8), ResBlock(16, 8), ResBlock(16, 8),
+#     nn.Conv2d(16, 8, 1), nn.Upsample(scale_factor=2),
+#     MemoryLayer('#3', True), nn.Conv2d(24, 8, 1),
+#     ResBlock(8, 8), ResBlock(8, 8), ResBlock(8, 8), ResBlock(8, 8),
+#     nn.Upsample(scale_factor=2),
+#     MemoryLayer('#2', True), nn.Conv2d(16, 8, 1),
+#     ResBlock(8, 8), ResBlock(8, 8), ResBlock(8, 8), ResBlock(8, 8),
+#     nn.Upsample(scale_factor=2),
+#     MemoryLayer('#1', True), nn.Conv2d(16, 8, 1),
+#     ResBlock(8, 8), ResBlock(8, 8), ResBlock(8, 8), ResBlock(8, 8),
+#     MemoryLayer('#0', True), nn.Conv2d(14, 8, 1),
+#     ResBlock(8, 8), ResBlock(8, 8), ResBlock(8, 8), ResBlock(8, 8),
+#     nn.Conv2d(8, 6, 1),
+# )
